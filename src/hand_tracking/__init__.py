3b972d1475c7e78f00a9ac494e58edcf57555ec2
Kangminseong <rkdalstjd0413@cau.ac.kr>
2025-12-11 21:34:16 +0900

    mobilenet

--- src/hand_tracking/__init__.py
+++ src/hand_tracking/__init__.py
@@ -1,9 +1,12 @@
 # Hand & Face Tracking Pipeline
-# - Hand: YOLO11n-Pose (21 keypoints) from chrismuntean/YOLO11n-pose-hands
+# - Hand Detection: YOLO11n-pose
+# - Hand Keypoint: MobileHand (MobileNetV3-Small) - Pruning/Quantization 지원
 # - Face: MediaPipe Face Mesh (468 keypoints)
 
-from .pipeline import HandTrackingPipeline, draw_landmarks, draw_detections
+from .pipeline_mobilehand import HandTrackingPipeline, draw_landmarks, draw_detections
 
-print("[hand_tracking] Using YOLO11n-Pose + MediaPipe Face Mesh")
+print("[hand_tracking] Using YOLO + MobileHand + MediaPipe Face")
 
 __all__ = ["HandTrackingPipeline", "draw_landmarks", "draw_detections"]
+
+
